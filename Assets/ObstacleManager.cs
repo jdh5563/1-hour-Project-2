b9357228c589bc7399cf67b4c1207dcac643cefc
jdh5563 <56982973+jdh5563@users.noreply.github.com>
2020-08-09 01:36:22 -0400

    Initial commit

---
+++ Assets/ObstacleManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+
+	public GameObject obstaclePrefab;
+	public Canvas canvas;
+	private float currentTime1;
+	private float currentTime2;
+	private Vector2 speed;
+	private float height;
+	private float delay;
+	private List<GameObject> obstacles;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		speed = new Vector2(-5, 0);
+		delay = 1.75f;
+		obstacles = new List<GameObject>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (currentTime1 < Time.timeSinceLevelLoad)
+		{
+			currentTime1 = Time.timeSinceLevelLoad + delay;
+			delay = Random.Range(1f, 3f);
+
+			height = Random.Range(100, 201);
+			obstacles.Add(Instantiate(obstaclePrefab));
+			obstacles[obstacles.Count - 1].transform.localScale = new Vector2(obstaclePrefab.transform.localScale.x, height);
+			obstacles[obstacles.Count - 1].GetComponent<Rigidbody2D>().velocity = speed;
+		}
+
+		if(currentTime2 < Time.timeSinceLevelLoad)
+		{
+			currentTime2 = Time.timeSinceLevelLoad + delay;
+			delay = Random.Range(1f, 2f);
+			height = Random.Range(100, 201);
+			obstacles.Add(Instantiate(obstaclePrefab));
+			obstacles[obstacles.Count - 1].transform.position = new Vector2(obstaclePrefab.transform.position.x + 5, -3.5f);
+			obstacles[obstacles.Count - 1].transform.localScale = new Vector2(obstaclePrefab.transform.localScale.x, height);
+			obstacles[obstacles.Count - 1].GetComponent<Rigidbody2D>().velocity = speed;
+		}
+
+		if(obstacles.Count > 0 && obstacles[0].transform.position.x < -10)
+		{
+			Destroy(obstacles[0]);
+			obstacles.RemoveAt(0);
+		}
+	}
+
+	public void Restart()
+	{
+		foreach(GameObject obj in obstacles)
+		{
+			Destroy(obj);
+		}
+
+		obstacles.Clear();
+		delay = 1.75f;
+		Time.timeScale = 1;
+		canvas.enabled = false;
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool hasJumped;
+    public GameObject mouseLeft;
+    public GameObject mouseRight;
+    public Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasJumped)
+        {
+            if (Input.GetMouseButtonDown(0) && gameObject.name == "Player 2")
+            {
+                rb.velocity = new Vector2(0, 12.5f);
+                hasJumped = true;
+            }
+
+            if (Input.GetMouseButtonDown(1) && gameObject.name == "Player 1")
+            {
+                rb.velocity = new Vector2(0, 12.5f);
+                hasJumped = true;
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            mouseLeft.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            mouseLeft.GetComponent<SpriteRenderer>().color = Color.black;
+
+            if (rb.velocity.y > 7 && gameObject.name == "Player 2")
+            {
+                rb.velocity = new Vector2(0, 7f);
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            mouseRight.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            mouseRight.GetComponent<SpriteRenderer>().color = Color.black;
+
+            if (rb.velocity.y > 7 && gameObject.name == "Player 1")
+            {
+                rb.velocity = new Vector2(0, 7f);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hasJumped = false;
+
+        if(collision.collider.tag == "Obstacle")
+        {
+            Time.timeScale = 0;
+            canvas.enabled = true;
+        }
+    }
+}
